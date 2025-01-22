e6a31b418776923605d14cf5822cb385ce48ba57
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-01-22 20:30:58 -0300

    Initial commit

---
+++ src/test/java/com/guilhermetotti/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermetotti.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
